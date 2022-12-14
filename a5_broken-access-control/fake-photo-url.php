407b7005f795546bad8277c6c0ff4dea65500f96
Paiboon Panusbordee <paiboonpa@gmail.com>
2022-12-14 22:37:21 +0700

    Update code for fake-photo-url

--- a5_broken-access-control/fake-photo-url.php
+++ a5_broken-access-control/fake-photo-url.php
@@ -2,8 +2,10 @@
 session_start();
 
 // จงเขียน code เพื่อเช็คว่าเป็น user ที่ login แล้วหรือไม่จากตัวแปร $_SESSION['user_id'] ว่ามีค่าเป็น 1 หรือไม่
+if ($_SESSION['user_id'] == 1) {
+    $url = "forbidden/identity-cardno-".$_GET['id'].".jpg";
+}
 
-$url = "forbidden/identity-cardno-".$_GET['id'].".jpg";
 if (file_exists($url)) {
     header("Content-type: image/jpeg");
     echo file_get_contents($url);
