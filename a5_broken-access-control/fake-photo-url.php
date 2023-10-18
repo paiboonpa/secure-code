b319d5753165273cc30cbd6fe7b3695243299e7e
Paiboon Panusbordee <paiboonpa@gmail.com>
2023-10-18 15:33:54 +0700

    comment first

--- a5_broken-access-control/fake-photo-url.php
+++ a5_broken-access-control/fake-photo-url.php
@@ -2,9 +2,9 @@
 session_start();
 
 // จงเขียน code เพื่อเช็คว่าเป็น user ที่ login แล้วหรือไม่จากตัวแปร $_SESSION['user_id'] ว่ามีค่าเป็น 1 หรือไม่
-if ($_SESSION['user_id'] == 1) {
+//if (!empty($_SESSION['user_id']) && $_SESSION['user_id'] == 1) {
     $url = "forbidden/identity-cardno-".$_GET['id'].".jpg";
-}
+//}
 
 if (file_exists($url)) {
     header("Content-type: image/jpeg");
